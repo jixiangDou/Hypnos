227033a8a0c3d58e5d0762e4a15f20e36f4d68f9
Jixiang Dou <jixiang.dou@gmail.com>
2021-02-27 02:00:45 +0900

    Project initialization.

---
+++ Hypnos/HypnosApp.swift
@@ -0,0 +1,17 @@
+//
+//  HypnosApp.swift
+//  Hypnos
+//
+//  Created by George Dou on 2021/02/27.
+//
+
+import SwiftUI
+
+@main
+struct HypnosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
